9ed5559e64557029c9eba721c3053c6d30d67ea7
jbilenko <jbilenko@fullsail.edu>
2015-10-22 13:14:45 -0400

    Having difficulties with functions
    I still don’t understand how to use a function.

--- Bilenko_Josh_Functions_Assignment/js/script.js
+++ Bilenko_Josh_Functions_Assignment/js/script.js
@@ -13,59 +13,38 @@ urWallet = Number(urWallet);
 
 var whereTo = confirm("Are you trying to go to the movies? Ok (Yes) Cancel (No)");
 
-// cost of items
-var movieTicket = 12;
-var avgfoodCost = 15;
-var rentMovie = 5;
-var lgPizza = 14;
-
 if (whereTo = true) {
   var howMany = prompt("How many people are going to the movies?");
   howMany = Number(howMany);
 } else {
-  var alert("So you've made you decisions to stay home! Congrats! Goodbye!");
+  alert("So you've made you decisions to stay home! Congrats! Goodbye!");
 }
 
-// Movie total function
 
-var movieTotal = movieCalc;
+// How many pizzas ultimate function
 
-function movieCalc (movieTicket, avgfoodCost, howMany) {
-  var movieCost = howMany * (movieTicket + avgfoodCost);
-  return movieCost
-}
+    function pizzaAmnt(howMany) {
+      var pizzaAmnt = howMany * (3/8);
+      console.log("You should get " + pizzaAmnt + " pizza(s) for " + howMany + " person(s).");
+    }
+
+var movieTicket = 12;
+var avgfoodCost = 15;
+var rentMovie = 5;
+var lgPizza = 14;
+var homeCost = rentMovie + (pizzaAmnt * lgPizza);
+var movieCost = howMany * (movieTicket + avgfoodCost);
+
 
 // conditional for amount of money you have vs movie total cost
 
-if (movieCost < urWallet) {
+if (urWallet > movieCost) {
   alert("You can afford to go to the movies!");
   console.log("You have $" + urWallet + " and going to the movies with " + howMany + " people will cost $" + movieCost + "!");
   console.log("Let's go to the movies!");
 } else {
-  alert("Do you even have enough money to stay home and rent a movie?");
-
-  if (homeCost < urWallet) {
-// How many pizzas ultimate function
-    function pizzaCalc (howMany) {
-      var pizzaAmnt = howMany * (3/8);
-      return pizzaAmnt
-    }
-
-    Math.round(pizzaAmnt);
-// home total cost function
-    function homeMovie (rentMovie, pizzaCalc, pizzaAmnt) {
-      var homeCost = rentMovie + pizzaAmnt;
-      return homeCost
-    }
-
     alert("Hey! Stay home an rent a movie!");
     console.log("You have $" + urWallet + " and staying home to rent a movie for " + howMany + " people costs $" + homeCost + "!");
-    console.log("Since you have " + howMany + " people staying over to watch a movie you need to get " + pizzaAmnt + " pizzas!");
+    pizzaAmnt(howMany);
     console.log("Enjoy your night a home!");
-  } else {
-    alert("You are poor.  Therefore stay home with the lights off to save money.");
-    alert("Enjoy your life in solitude for the next 6 hours!");
-    alert("Refresh to do this again!");
-  }
-
 }
